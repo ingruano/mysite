163d2787a50db03c7d1342d1a7739ae34f1eb62a
ingruano <andres.torres.marroquin@gmail.com>
2011-07-05 08:36:59 -0500

    first

---
+++ polls/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import *
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('polls.views',
+    (r'^$', 'index'),
+    (r'^(?P<poll_id>\d+)/$', 'detail'),
+    (r'^(?P<poll_id>\d+)/results/$', 'results'),
+    (r'^(?P<poll_id>\d+)/vote/$', 'vote'),
+)
+
